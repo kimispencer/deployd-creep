4fb5cb06586200d8e76ab9959baeba6aebd49a35
kimi spencer <kimispencer@gmail.com>
2014-01-30 15:19:19 +0800

    app.js db settings

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var deployd = require('deployd');
  
 var options = {
-  port:process.env.PORT || 3000
+  port:process.env.PORT || 3000,
+  db:{
+    port:10091,              // \
+    host:"troup.mongohq.com", //  \ 
+    name:"app21722030",      //   \
+    credentials:{            //   / replace with your own settings
+      username:"admin", //  /
+      password:"b00bies"  // /
+    }
+  }
 };
  
 var dpd = deployd(options);
